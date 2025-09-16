226d1434738112852d53c3839a866e1d201caa09
Brian Innes <binnes@users.noreply.github.com>
2025-09-16 15:47:25 +0000

    initial Orchestrate implementation

--- scripts/load_agents.sh
+++ scripts/load_agents.sh
@@ -1,2 +1,30 @@
 source .env
 orchestrate env activate local
+
+# Delete existing
+
+# Agents
+orchestrate agents remove -n movie_reviews -k native
+
+# Tools
+orchestrate tools remove -n MovieReviewRAG
+
+# Connections
+orchestrate connections remove --app-id watsonx
+orchestrate connections remove --app-id astraDB
+
+## Create 
+
+# Connections
+orchestrate connections import -f wxo/connections/astradb.yaml
+orchestrate connections import -f wxo/connections/watsonx.yaml
+
+# Credentials
+orchestrate connections set-credentials -a astraDB --environment draft -e ASTRA_DB_API_ENDPOINT=$ASTRA_DB_API_ENDPOINT -e ASTRA_DB_APPLICATION_TOKEN=$ASTRA_DB_APPLICATION_TOKEN
+orchestrate connections set-credentials -a watsonx --environment draft -e WATSONX_APIKEY=$WATSONX_APIKEY -e WATSONX_PROJECT_ID=$WATSONX_PROJECT_ID
+
+# Tool
+orchestrate tools import -k python -f wxo/tools/movie_review_RAG.py --app-id astraDB --app-id watsonx -r wxo/tools/requirements.txt
+
+# Agent
+orchestrate agents import -f wxo/agents/movie_reviews.yaml
---
+++ wxo/tools/movie_review_RAG.py
@@ -0,0 +1,99 @@
+from ibm_watsonx_orchestrate.agent_builder.tools import tool, ToolPermission
+from ibm_watsonx_orchestrate.run import connections
+from ibm_watsonx_orchestrate.agent_builder.connections import ConnectionType, ExpectedCredentials
+from langchain_astradb import AstraDBVectorStore
+from langchain_ibm import WatsonxEmbeddings
+from graph_retriever.strategies import Eager
+from langchain_graph_retriever import GraphRetriever
+
+
+
+@tool(name="MovieReviewRAG", 
+      permission=ToolPermission.READ_ONLY,
+      expected_credentials=[
+          ExpectedCredentials(
+            app_id = "astraDB",
+            type = ConnectionType.KEY_VALUE),
+          ExpectedCredentials(
+            app_id = "watsonx",
+            type = ConnectionType.KEY_VALUE)
+        ]
+)
+def movie_review_rag(question:str) -> str:
+    """
+    Retrieve data for answer to question using RAG
+    
+    Args:
+        question (str): The question to find answers for
+    Returns:
+        str: The candidate data for the answer
+    """
+
+    astraDB_conn = connections.key_value("astraDB")
+    watsonx_conn = connections.key_value("watsonx")
+
+    ASTRA_DB_API_ENDPOINT = astraDB_conn['ASTRA_DB_API_ENDPOINT']
+    ASTRA_DB_APPLICATION_TOKEN = astraDB_conn['ASTRA_DB_APPLICATION_TOKEN']
+    WATSONX_APIKEY = watsonx_conn['WATSONX_APIKEY']
+    WATSONX_PROJECT_ID = watsonx_conn['WATSONX_PROJECT_ID']
+    
+    embeddings = WatsonxEmbeddings(
+        model_id="ibm/slate-125m-english-rtrvr",
+        url="https://us-south.ml.cloud.ibm.com",
+        apikey=WATSONX_APIKEY,
+        project_id=WATSONX_PROJECT_ID,
+        api_endpoint=ASTRA_DB_API_ENDPOINT,
+        token=ASTRA_DB_APPLICATION_TOKEN
+    )
+
+    COLLECTION = "movie_reviews_rotten_tomatoes"
+    vectorstore = AstraDBVectorStore(
+        embedding=embeddings,
+        collection_name=COLLECTION,
+        pre_delete_collection=False,
+    )
+    
+    retriever = GraphRetriever(
+        store=vectorstore,
+        edges=[("reviewed_movie_id", "movie_id")],
+        strategy=Eager(start_k=10, adjacent_k=10, select_k=100, max_depth=1),
+    )
+    
+    # invoke the query
+    query_results = retriever.invoke(question)
+    
+    # collect the movie info for each film retrieved
+    compiled_results = {}
+    for result in query_results:
+        if result.metadata["doc_type"] == "movie_info":
+            movie_id = result.metadata["movie_id"]
+            movie_title = result.metadata["title"]
+            compiled_results[movie_id] = {
+                "movie_id": movie_id,
+                "movie_title": movie_title,
+                "reviews": {},
+            }
+
+    # go through the results a second time, collecting the retreived reviews for
+    # each of the movies
+    for result in query_results:
+        if result.metadata["doc_type"] == "movie_review":
+            reviewed_movie_id = result.metadata["reviewed_movie_id"]
+            review_id = result.metadata["reviewId"]
+            review_text = result.page_content
+            compiled_results[reviewed_movie_id]["reviews"][review_id] = review_text
+
+
+    # compile the retrieved movies and reviews into a string that we can pass to an
+    # LLM in an augmented prompt
+    formatted_text = ""
+    for movie_id, review_list in compiled_results.items():
+        formatted_text += "\n\n Movie Title: "
+        formatted_text += review_list["movie_title"]
+        formatted_text += "\n Movie ID: "
+        formatted_text += review_list["movie_id"]
+        for review_id, review_text in review_list["reviews"].items():
+            formatted_text += "\n Review: "
+            formatted_text += review_text
+
+    return formatted_text
